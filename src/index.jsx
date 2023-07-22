3454b3dc73dce5c4b00fb186336abd35d5b08b17
Pablo Madrigal <pablo.madrigal@excelsoftsources.com>
2023-07-22 00:12:26 -0600

    Hash router

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,18 @@
-import * as ReactDOM from 'react-dom/client';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import App from './App';
-import theme from './theme';
+import * as ReactDOM from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import App from './App'
+import theme from './theme'
+import { HashRouter } from 'react-router-dom'
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
+const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(rootElement)
 
 root.render(
   <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
-);
+    <HashRouter>
+      <CssBaseline />
+      <App />
+    </HashRouter>
+  </ThemeProvider>
+)
