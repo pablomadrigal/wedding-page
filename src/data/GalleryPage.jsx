bef600771f66eeee8b07fd32952477ed22a47216
Pablo Madrigal <pablo.madrigal@excelsoftsources.com>
2023-11-02 19:10:53 -0600

    Add 360 videos

---
+++ src/data/GalleryPage.jsx
@@ -0,0 +1,560 @@
+export const videos360 = [
+  {
+    id: 1,
+    name: 'video360-1',
+    url: 'https://pabloypaloma.site/assent/videos/video360-1.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-1-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-1-thumbnail.png',
+  },
+  {
+    id: 2,
+    name: 'video360-2',
+    url: 'https://pabloypaloma.site/assent/videos/video360-2.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-2-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-2-thumbnail.png',
+  },
+  {
+    id: 3,
+    name: 'video360-3',
+    url: 'https://pabloypaloma.site/assent/videos/video360-3.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-3-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-3-thumbnail.png',
+  },
+  {
+    id: 4,
+    name: 'video360-4',
+    url: 'https://pabloypaloma.site/assent/videos/video360-4.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-4-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-4-thumbnail.png',
+  },
+  {
+    id: 5,
+    name: 'video360-5',
+    url: 'https://pabloypaloma.site/assent/videos/video360-5.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-5-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-5-thumbnail.png',
+  },
+  {
+    id: 6,
+    name: 'video360-6',
+    url: 'https://pabloypaloma.site/assent/videos/video360-6.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-6-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-6-thumbnail.png',
+  },
+  {
+    id: 7,
+    name: 'video360-7',
+    url: 'https://pabloypaloma.site/assent/videos/video360-7.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-7-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-7-thumbnail.png',
+  },
+  {
+    id: 8,
+    name: 'video360-8',
+    url: 'https://pabloypaloma.site/assent/videos/video360-8.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-8-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-8-thumbnail.png',
+  },
+  {
+    id: 9,
+    name: 'video360-9',
+    url: 'https://pabloypaloma.site/assent/videos/video360-9.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-9-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-9-thumbnail.png',
+  },
+  {
+    id: 10,
+    name: 'video360-10',
+    url: 'https://pabloypaloma.site/assent/videos/video360-10.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-10-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-10-thumbnail.png',
+  },
+  {
+    id: 11,
+    name: 'video360-11',
+    url: 'https://pabloypaloma.site/assent/videos/video360-11.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-11-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-11-thumbnail.png',
+  },
+  {
+    id: 12,
+    name: 'video360-12',
+    url: 'https://pabloypaloma.site/assent/videos/video360-12.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-12-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-12-thumbnail.png',
+  },
+  {
+    id: 13,
+    name: 'video360-13',
+    url: 'https://pabloypaloma.site/assent/videos/video360-13.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-13-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-13-thumbnail.png',
+  },
+  {
+    id: 14,
+    name: 'video360-14',
+    url: 'https://pabloypaloma.site/assent/videos/video360-14.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-14-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-14-thumbnail.png',
+  },
+  {
+    id: 15,
+    name: 'video360-15',
+    url: 'https://pabloypaloma.site/assent/videos/video360-15.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-15-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-15-thumbnail.png',
+  },
+  {
+    id: 16,
+    name: 'video360-16',
+    url: 'https://pabloypaloma.site/assent/videos/video360-16.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-16-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-16-thumbnail.png',
+  },
+  {
+    id: 17,
+    name: 'video360-17',
+    url: 'https://pabloypaloma.site/assent/videos/video360-17.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-17-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-17-thumbnail.png',
+  },
+  {
+    id: 18,
+    name: 'video360-18',
+    url: 'https://pabloypaloma.site/assent/videos/video360-18.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-18-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-18-thumbnail.png',
+  },
+  {
+    id: 19,
+    name: 'video360-19',
+    url: 'https://pabloypaloma.site/assent/videos/video360-19.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-19-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-19-thumbnail.png',
+  },
+  {
+    id: 20,
+    name: 'video360-20',
+    url: 'https://pabloypaloma.site/assent/videos/video360-20.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-20-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-20-thumbnail.png',
+  },
+  {
+    id: 21,
+    name: 'video360-21',
+    url: 'https://pabloypaloma.site/assent/videos/video360-21.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-21-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-21-thumbnail.png',
+  },
+  {
+    id: 22,
+    name: 'video360-22',
+    url: 'https://pabloypaloma.site/assent/videos/video360-22.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-22-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-22-thumbnail.png',
+  },
+  {
+    id: 23,
+    name: 'video360-23',
+    url: 'https://pabloypaloma.site/assent/videos/video360-23.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-23-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-23-thumbnail.png',
+  },
+  {
+    id: 24,
+    name: 'video360-24',
+    url: 'https://pabloypaloma.site/assent/videos/video360-24.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-24-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-24-thumbnail.png',
+  },
+  {
+    id: 25,
+    name: 'video360-25',
+    url: 'https://pabloypaloma.site/assent/videos/video360-25.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-25-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-25-thumbnail.png',
+  },
+  {
+    id: 26,
+    name: 'video360-26',
+    url: 'https://pabloypaloma.site/assent/videos/video360-26.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-26-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-26-thumbnail.png',
+  },
+  {
+    id: 27,
+    name: 'video360-27',
+    url: 'https://pabloypaloma.site/assent/videos/video360-27.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-27-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-27-thumbnail.png',
+  },
+  {
+    id: 28,
+    name: 'video360-28',
+    url: 'https://pabloypaloma.site/assent/videos/video360-28.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-28-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-28-thumbnail.png',
+  },
+  {
+    id: 29,
+    name: 'video360-29',
+    url: 'https://pabloypaloma.site/assent/videos/video360-29.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-29-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-29-thumbnail.png',
+  },
+  {
+    id: 30,
+    name: 'video360-30',
+    url: 'https://pabloypaloma.site/assent/videos/video360-30.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-30-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-30-thumbnail.png',
+  },
+  {
+    id: 31,
+    name: 'video360-31',
+    url: 'https://pabloypaloma.site/assent/videos/video360-31.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-31-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-31-thumbnail.png',
+  },
+  {
+    id: 32,
+    name: 'video360-32',
+    url: 'https://pabloypaloma.site/assent/videos/video360-32.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-32-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-32-thumbnail.png',
+  },
+  {
+    id: 33,
+    name: 'video360-33',
+    url: 'https://pabloypaloma.site/assent/videos/video360-33.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-33-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-33-thumbnail.png',
+  },
+  {
+    id: 34,
+    name: 'video360-34',
+    url: 'https://pabloypaloma.site/assent/videos/video360-34.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-34-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-34-thumbnail.png',
+  },
+  {
+    id: 35,
+    name: 'video360-35',
+    url: 'https://pabloypaloma.site/assent/videos/video360-35.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-35-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-35-thumbnail.png',
+  },
+  {
+    id: 36,
+    name: 'video360-36',
+    url: 'https://pabloypaloma.site/assent/videos/video360-36.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-36-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-36-thumbnail.png',
+  },
+  {
+    id: 37,
+    name: 'video360-37',
+    url: 'https://pabloypaloma.site/assent/videos/video360-37.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-37-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-37-thumbnail.png',
+  },
+  {
+    id: 38,
+    name: 'video360-38',
+    url: 'https://pabloypaloma.site/assent/videos/video360-38.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-38-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-38-thumbnail.png',
+  },
+  {
+    id: 39,
+    name: 'video360-39',
+    url: 'https://pabloypaloma.site/assent/videos/video360-39.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-39-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-39-thumbnail.png',
+  },
+  {
+    id: 40,
+    name: 'video360-40',
+    url: 'https://pabloypaloma.site/assent/videos/video360-40.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-40-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-40-thumbnail.png',
+  },
+  {
+    id: 41,
+    name: 'video360-41',
+    url: 'https://pabloypaloma.site/assent/videos/video360-41.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-41-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-41-thumbnail.png',
+  },
+  {
+    id: 42,
+    name: 'video360-42',
+    url: 'https://pabloypaloma.site/assent/videos/video360-42.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-42-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-42-thumbnail.png',
+  },
+  {
+    id: 43,
+    name: 'video360-43',
+    url: 'https://pabloypaloma.site/assent/videos/video360-43.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-43-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-43-thumbnail.png',
+  },
+  {
+    id: 44,
+    name: 'video360-44',
+    url: 'https://pabloypaloma.site/assent/videos/video360-44.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-44-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-44-thumbnail.png',
+  },
+  {
+    id: 45,
+    name: 'video360-45',
+    url: 'https://pabloypaloma.site/assent/videos/video360-45.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-45-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-45-thumbnail.png',
+  },
+  {
+    id: 46,
+    name: 'video360-46',
+    url: 'https://pabloypaloma.site/assent/videos/video360-46.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-46-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-46-thumbnail.png',
+  },
+  {
+    id: 47,
+    name: 'video360-47',
+    url: 'https://pabloypaloma.site/assent/videos/video360-47.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-47-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-47-thumbnail.png',
+  },
+  {
+    id: 48,
+    name: 'video360-48',
+    url: 'https://pabloypaloma.site/assent/videos/video360-48.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-48-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-48-thumbnail.png',
+  },
+  {
+    id: 49,
+    name: 'video360-49',
+    url: 'https://pabloypaloma.site/assent/videos/video360-49.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-49-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-49-thumbnail.png',
+  },
+  {
+    id: 50,
+    name: 'video360-50',
+    url: 'https://pabloypaloma.site/assent/videos/video360-50.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-50-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-50-thumbnail.png',
+  },
+  {
+    id: 51,
+    name: 'video360-51',
+    url: 'https://pabloypaloma.site/assent/videos/video360-51.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-51-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-51-thumbnail.png',
+  },
+  {
+    id: 52,
+    name: 'video360-52',
+    url: 'https://pabloypaloma.site/assent/videos/video360-52.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-52-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-52-thumbnail.png',
+  },
+  {
+    id: 53,
+    name: 'video360-53',
+    url: 'https://pabloypaloma.site/assent/videos/video360-53.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-53-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-53-thumbnail.png',
+  },
+  {
+    id: 54,
+    name: 'video360-54',
+    url: 'https://pabloypaloma.site/assent/videos/video360-54.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-54-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-54-thumbnail.png',
+  },
+  {
+    id: 55,
+    name: 'video360-55',
+    url: 'https://pabloypaloma.site/assent/videos/video360-55.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-55-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-55-thumbnail.png',
+  },
+  {
+    id: 56,
+    name: 'video360-56',
+    url: 'https://pabloypaloma.site/assent/videos/video360-56.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-56-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-56-thumbnail.png',
+  },
+  {
+    id: 57,
+    name: 'video360-57',
+    url: 'https://pabloypaloma.site/assent/videos/video360-57.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-57-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-57-thumbnail.png',
+  },
+  {
+    id: 58,
+    name: 'video360-58',
+    url: 'https://pabloypaloma.site/assent/videos/video360-58.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-58-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-58-thumbnail.png',
+  },
+  {
+    id: 59,
+    name: 'video360-59',
+    url: 'https://pabloypaloma.site/assent/videos/video360-59.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-59-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-59-thumbnail.png',
+  },
+  {
+    id: 60,
+    name: 'video360-60',
+    url: 'https://pabloypaloma.site/assent/videos/video360-60.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-60-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-60-thumbnail.png',
+  },
+  {
+    id: 61,
+    name: 'video360-61',
+    url: 'https://pabloypaloma.site/assent/videos/video360-61.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-61-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-61-thumbnail.png',
+  },
+  {
+    id: 62,
+    name: 'video360-62',
+    url: 'https://pabloypaloma.site/assent/videos/video360-62.mp4',
+    minUrl:
+      'https://pabloypaloma.site/assent/videos/Compress/video360-62-min.mp4',
+    thumbnail:
+      'https://pabloypaloma.site/assent/videos/thumbnails/video360-62-thumbnail.png',
+  },
+]
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,8 +1,10 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import BasicLayout from '../components/layout/BasicLayout'
 
 import FlowerDivider from '../components/image/FlowerDivider'
 import Portada from '../assents/portadas/Proximamente-min.png'
+import { videos360 } from '../data/GalleryPage'
+import ReactPlayer from 'react-player'
 
 const Gallery = () => {
   return (
@@ -20,9 +22,28 @@ const Gallery = () => {
           variant="h2"
           sx={{ mt: 5, fontFamily: 'Alex Brush', textAlign: 'center' }}
         >
-          Próximamente
+          Videos 360°
         </Typography>
       </Box>
+      <Grid container>
+        {videos360.map((video) => (
+          <Grid item key={video.id} xs={6} sm={4} md={3} lg={3} xl={2}>
+            <ReactPlayer
+              width="100%"
+              height="auto"
+              url={video.url}
+              controls
+              light={
+                <img
+                  src={video.thumbnail}
+                  alt={video.name}
+                  style={{ width: '100%' }}
+                />
+              }
+            />
+          </Grid>
+        ))}
+      </Grid>
       <FlowerDivider onlyMobile position="up" />
     </BasicLayout>
   )
